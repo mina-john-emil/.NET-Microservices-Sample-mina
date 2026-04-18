139f02409f1aa045efa0abdb8334ce01c5493a09
mina john emil <minajohn949@gmail.com>
2026-04-18 23:39:41 +0200

    Update CorrectForLocalK8s.sh

--- CorrectForLocalK8s.sh
+++ CorrectForLocalK8s.sh
@@ -1,6 +1,5 @@
 sed -i 's#image: microservices/adminfrontend#image: microservices/microservicesadminfrontend#g' k8s/adminfrontend.yaml
 sed -i 's#image: microservices/webfrontend#image: microservices/microserviceswebfrontend#g' k8s/webfrontend.yaml
-sed -i 's#image: microservices/paymentservice#image: microservices/paymentservice#g' k8s/paymentservice.yaml
 
 # This loop adds ":latest" to all the microservice YAMLs
 for file in k8s/adminfrontend.yaml k8s/apigatewayadmin.yaml k8s/apigatewayforweb.yaml k8s/basketservice.yaml k8s/discountservice.yaml k8s/identityservice.yaml k8s/orderservice.yaml k8s/paymentservice.yaml k8s/productservice.yaml k8s/webfrontend.yaml; do
